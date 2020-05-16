0401c27a22d267c380e8a277caf7c61f4d93ac3a
Lopeesss <danilosousalopes@gmail.com>
2020-05-16 17:52:05 -0300

    Adding gallery and error control to the matches

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -16,7 +16,9 @@ namespace DatingApp.API.Helpers
                     opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<User, UserForDetailedDto>()
                 .ForMember(dest => dest.PhotoUrl, opt => 
-                opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url));
+                opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt=>
+                    opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotosForDetailedDto>();
         }
     }
